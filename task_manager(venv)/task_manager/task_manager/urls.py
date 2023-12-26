79fefe9667b96437038d2c01438f121338687c45
S M Simon <koushiksimon@gmail.com>
2023-12-27 00:27:16 +0600

    userauthentication

--- task_manager(venv)/task_manager/task_manager/urls.py
+++ task_manager(venv)/task_manager/task_manager/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static 
 from . import views  # Lowercase 'views'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('signup/',views.signup,name='signup'),
-    path('login/',views.login,name='login'),
+    path('login/',views.signin,name='signin'),
 
-]
+] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) # media file serving configuration 
 
--- task_manager(venv)/task_manager/task_manager/views.py
+++ task_manager(venv)/task_manager/task_manager/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -6,8 +9,45 @@ def index(request):
     return render(request,'index.html',context={})
 
 def signup(request):
-    return render(request,'registration/signup.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
-def login(request):
+        # Check if username already exists
+        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists() :
+            messages.error(request, 'Username or email already exists. Please try a different one.')
+            return redirect('signup')  # Redirect back to signup page
+
+        # Create a new User using Django's create_user method
+        user = User (
+            username=username, 
+            email=email,            
+        )
+        user.set_password(password)
+        user.save()
+        messages.success(request, 'Account created successfully! Please login.')
+        return redirect('signin')  # Redirect to login page after successful signup
+    else:
+        return render(request, 'registration/signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            # Log the user in
+            login(request, user)
+            messages.success(request, 'Logged in successfully!')
+            return redirect('index')  # Redirect to some page after successful login
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('signin')  # Redirect back to login page for retry
+
     return render(request,'registration/login.html')
 
